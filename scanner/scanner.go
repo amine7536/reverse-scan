5c2c53e0c2a8ec530e46a954e8f7c6b554264186
Amine Benseddik <amine.benseddik@gmail.com>
2017-06-29 12:01:47 +0200

    Adding progressbar and mock data

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -4,6 +4,9 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"time"
+
+	"github.com/gosuri/uiprogress"
 
 	"bitbucket.org/aminebenseddik/reverse-scan/conf"
 	"bitbucket.org/aminebenseddik/reverse-scan/utils"
@@ -29,6 +32,11 @@ func Start(config *conf.Config) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
+	uiprogress.Start()                   // start rendering
+	bar := uiprogress.AddBar(len(mynet)) // Add a new bar
+	bar.AppendCompleted()
+	bar.PrependElapsed()
+
 	go rangeHosts(mynet, reverseChan, flushChan)
 
 mainloop:
@@ -39,6 +47,7 @@ mainloop:
 			if err != nil {
 				log.Fatal(err)
 			}
+			bar.Incr()
 		case <-flushChan:
 			writer.Flush()
 			break mainloop
@@ -49,9 +58,11 @@ mainloop:
 
 func rangeHosts(hosts []string, reverseChan chan []string, flushChan chan bool) {
 	for _, ip := range hosts {
-		names, _ := utils.ResolveName(ip)
+		// names, _ := utils.ResolveName(ip)
 		// reverseChan <- append([]string{ip}, names...)
+		names := []string{"toto.com"}
 		reverseChan <- append([]string{ip}, names...)
+		time.Sleep(time.Millisecond * 20)
 
 		// err := writer.Write(append([]string{ip}, names...))
 		// if err != nil {
